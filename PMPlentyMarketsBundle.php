e2f4d065b0b35958b48007e5e88339e44f63f6ea
Sven Joder <sj@pmdevelopment.de>
2026-06-16 14:36:38 +0200

    Remove deprecations

--- PMPlentyMarketsBundle.php
+++ PMPlentyMarketsBundle.php
@@ -4,11 +4,6 @@ namespace PM\PlentyMarketsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-/**
- * Class PMPlentyMarketsBundle
- *
- * @package PM\PlentyMarketsBundle
- */
 class PMPlentyMarketsBundle extends Bundle
 {
 }
